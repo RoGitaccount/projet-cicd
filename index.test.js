29d00c3f152f5512582f7742dafef58230786a41
RoGitaccount <162996846+RoGitaccount@users.noreply.github.com>
2025-03-28 17:54:00 +0100

    Revert "test: verification du workflow"

--- index.test.js
+++ index.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const app = require('./index');
 
 describe('API Tests', () => {
-  test('GET / doir retourner \"Test du workflow CI/CD\"', async () => {
+  test('GET / doir retourner \"Bonjour la branche Fonctionnalité !\"', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ message: 'Test du workflow CI/CD' });
+    expect(response.body).toEqual({ message: 'Bonjour la branche Fonctionnalité !' });
   });
 
   test('GET /api/status doir retourner OK', async () => {
