a90d9061b053438d85847f0aef66faebfcbbc759
RoGitaccount <rayanollivier0@gmail.com>
2025-03-28 15:26:39 +0100

    modification du main.yml pour le deploy

--- index.test.js
+++ index.test.js
@@ -15,7 +15,7 @@ describe('API Tests', () => {
   });
 
 
-  test('GET /start doir retourner START', async () => {
+  test('GET /start doir retourner STOP', async () => {
     const response = await request(app).get('/start');
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({ message: 'STOP' });
